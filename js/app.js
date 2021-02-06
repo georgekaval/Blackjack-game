64c32bfcb71817e7c71d30884ffa0b384b5b270a
George_Kavallieratos <georgealkaval@gmail.com>
2021-02-06 16:53:11 -0500

    updated event listeners

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 const defineDeck =[
   {card: 'A', value: 1, suit: 'heart'}, {card: 'A', value: 1, suit: 'spade'}, {card: 'A', value: 1, suit: 'diamond'}, {card: 'A', value: 1, suit: 'clubs'}, {card: 2, value: 2, suit: 'heart'}, {card: 2, value: 2, suit: 'spade'}, {card: 2, value: 2, suit: 'diamond'}, {card: 2, value: 2, suit: 'clubs'}, {card: 3, value: 3, suit: 'heart'}, {card: 3, value: 3, suit: 'spade'}, {card: 3, value: 3, suit: 'diamond'}, {card: 3, value: 3, suit: 'clubs'}, {card: 4, value: 4, suit: 'heart'}, {card: 4, value: 4, suit: 'spade'}, {card: 4, value: 4, suit: 'diamond'}, {card: 4, value: 4, suit: 'clubs'}, {card: 5, value: 5, suit: 'heart'}, {card: 5, value: 5, suit: 'spade'}, {card: 5, value: 5, suit: 'diamond'}, {card: 5, value: 5, suit: 'clubs'}, {card: 6, value: 6, suit: 'heart'}, {card: 6, value: 6, suit: 'spade'}, {card: 6, value: 6, suit: 'diamond'}, {card: 6, value: 6, suit: 'clubs'}, {card: 7, value: 7, suit: 'heart'}, {card: 7, value: 7, suit: 'spade'}, {card: 7, value: 7, suit: 'diamond'}, {card: 7, value: 7, suit: 'clubs'}, {card: 8, value: 8, suit: 'heart'}, {card: 8, value: 8, suit: 'spade'}, {card: 8, value: 8, suit: 'diamond'}, {card: 8, value: 8, suit: 'clubs'}, {card: 9, value: 9, suit: 'heart'}, {card: 9, value: 9, suit: 'spade'}, {card: 9, value: 9, suit: 'diamond'}, {card: 9, value: 9, suit: 'clubs'}, {card: 10, value: 10, suit: 'heart'}, {card: 10, value: 10, suit: 'spade'}, {card: 10, value: 10, suit: 'diamond'}, {card: 10, value: 10, suit: 'clubs'}, {card: 'J', value: 10, suit: 'heart'}, {card: 'J', value: 10, suit: 'spade'}, {card: 'J', value: 10, suit: 'diamond'}, {card: 'J', value: 10, suit: 'clubs'}, {card: 'Q', value: 10, suit: 'heart'}, {card: 'Q', value: 10, suit: 'spade'}, {card: 'Q', value: 10, suit: 'diamond'}, {card: 'Q', value: 10, suit: 'clubs'}, {card: 'K', value: 10, suit: 'heart'}, {card: 'K', value: 10, suit: 'spade'}, {card: 'K', value: 10, suit: 'diamond'}, {card: 'K', value: 10, suit: 'clubs'}
 ]
-console.log(defineDeck[4].value);
 
 //build the deck with random cards
 const randomizeDeck = () => {
@@ -11,8 +10,7 @@ const randomizeDeck = () => {
     [defineDeck[i],defineDeck[j]] = [defineDeck[j], defineDeck[i]]
   }
 }
-// randomizeDeck()
-console.log(defineDeck);
+
 
 //Have the computer choose the first 2 cards for itself and the user
 let computerCards = []
@@ -21,26 +19,20 @@ const dealCards = () => {
   computerCards = defineDeck.splice(0,2)
   userCards = defineDeck.splice(0,2)
 }
-// chooseCardsToDeal()
-console.log(computerCards);
-console.log(userCards);
-console.log(defineDeck);
 
 //Choose another card, this is called in the hitMeCheckTwentyOne function
 const hitMeBasic = (who) => {
   who.push(...defineDeck.splice(0,1))
 }
 
-
 //Have the computer count the cards values in a hand
 let sum = 0
 const countCardValue = (who) => {
+  sum = 0
   for(let i = 0; i < who.length; i ++){
     sum += who[i].value
   }return sum
 }
-// countCardValue(userCards)
-console.log(sum);
 
 //Have the computer check when the hit me button is clicked, if you passed 21 and lost
 const hitMeCheckTwentyOne = (who) => {
@@ -51,9 +43,6 @@ const hitMeCheckTwentyOne = (who) => {
     alert('Over 21, good try!')
   }
 }
-// hitMeCheckTwentyOne(userCards)
-console.log(userCards);
-console.log(sum);
 
 //Have the computer compare the two hands and see who won, if no one hit over 21 already
 const checkHands = () => {
@@ -71,7 +60,6 @@ const checkHands = () => {
     alert('You tied!')
   }
 }
-// checkHands()
 
 //Make the Start game button shuffle the deck
 $(() => {

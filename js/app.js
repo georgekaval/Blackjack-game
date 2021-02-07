0ba63542089eec94a15adc1b7702464217bc56fe
George_Kavallieratos <georgealkaval@gmail.com>
2021-02-07 17:35:08 -0500

    Made it so the type of cards show up when you click deal

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 //Make an array with all the cards as objects
 const defineDeck =[
-  {card: 'A', value: 1, suit: 'heart'}, {card: 'A', value: 1, suit: 'spade'}, {card: 'A', value: 1, suit: 'diamond'}, {card: 'A', value: 1, suit: 'clubs'}, {card: 2, value: 2, suit: 'heart'}, {card: 2, value: 2, suit: 'spade'}, {card: 2, value: 2, suit: 'diamond'}, {card: 2, value: 2, suit: 'clubs'}, {card: 3, value: 3, suit: 'heart'}, {card: 3, value: 3, suit: 'spade'}, {card: 3, value: 3, suit: 'diamond'}, {card: 3, value: 3, suit: 'clubs'}, {card: 4, value: 4, suit: 'heart'}, {card: 4, value: 4, suit: 'spade'}, {card: 4, value: 4, suit: 'diamond'}, {card: 4, value: 4, suit: 'clubs'}, {card: 5, value: 5, suit: 'heart'}, {card: 5, value: 5, suit: 'spade'}, {card: 5, value: 5, suit: 'diamond'}, {card: 5, value: 5, suit: 'clubs'}, {card: 6, value: 6, suit: 'heart'}, {card: 6, value: 6, suit: 'spade'}, {card: 6, value: 6, suit: 'diamond'}, {card: 6, value: 6, suit: 'clubs'}, {card: 7, value: 7, suit: 'heart'}, {card: 7, value: 7, suit: 'spade'}, {card: 7, value: 7, suit: 'diamond'}, {card: 7, value: 7, suit: 'clubs'}, {card: 8, value: 8, suit: 'heart'}, {card: 8, value: 8, suit: 'spade'}, {card: 8, value: 8, suit: 'diamond'}, {card: 8, value: 8, suit: 'clubs'}, {card: 9, value: 9, suit: 'heart'}, {card: 9, value: 9, suit: 'spade'}, {card: 9, value: 9, suit: 'diamond'}, {card: 9, value: 9, suit: 'clubs'}, {card: 10, value: 10, suit: 'heart'}, {card: 10, value: 10, suit: 'spade'}, {card: 10, value: 10, suit: 'diamond'}, {card: 10, value: 10, suit: 'clubs'}, {card: 'J', value: 10, suit: 'heart'}, {card: 'J', value: 10, suit: 'spade'}, {card: 'J', value: 10, suit: 'diamond'}, {card: 'J', value: 10, suit: 'clubs'}, {card: 'Q', value: 10, suit: 'heart'}, {card: 'Q', value: 10, suit: 'spade'}, {card: 'Q', value: 10, suit: 'diamond'}, {card: 'Q', value: 10, suit: 'clubs'}, {card: 'K', value: 10, suit: 'heart'}, {card: 'K', value: 10, suit: 'spade'}, {card: 'K', value: 10, suit: 'diamond'}, {card: 'K', value: 10, suit: 'clubs'}
+  {card: 'A', value: 1, suit: 'hearts'}, {card: 'A', value: 1, suit: 'spades'}, {card: 'A', value: 1, suit: 'diamonds'}, {card: 'A', value: 1, suit: 'clubs'}, {card: 2, value: 2, suit: 'hearts'}, {card: 2, value: 2, suit: 'spades'}, {card: 2, value: 2, suit: 'diamonds'}, {card: 2, value: 2, suit: 'clubs'}, {card: 3, value: 3, suit: 'hearts'}, {card: 3, value: 3, suit: 'spades'}, {card: 3, value: 3, suit: 'diamonds'}, {card: 3, value: 3, suit: 'clubs'}, {card: 4, value: 4, suit: 'hearts'}, {card: 4, value: 4, suit: 'spades'}, {card: 4, value: 4, suit: 'diamonds'}, {card: 4, value: 4, suit: 'clubs'}, {card: 5, value: 5, suit: 'hearts'}, {card: 5, value: 5, suit: 'spades'}, {card: 5, value: 5, suit: 'diamonds'}, {card: 5, value: 5, suit: 'clubs'}, {card: 6, value: 6, suit: 'hearts'}, {card: 6, value: 6, suit: 'spades'}, {card: 6, value: 6, suit: 'diamonds'}, {card: 6, value: 6, suit: 'clubs'}, {card: 7, value: 7, suit: 'hearts'}, {card: 7, value: 7, suit: 'spades'}, {card: 7, value: 7, suit: 'diamonds'}, {card: 7, value: 7, suit: 'clubs'}, {card: 8, value: 8, suit: 'hearts'}, {card: 8, value: 8, suit: 'spades'}, {card: 8, value: 8, suit: 'diamonds'}, {card: 8, value: 8, suit: 'clubs'}, {card: 9, value: 9, suit: 'hearts'}, {card: 9, value: 9, suit: 'spades'}, {card: 9, value: 9, suit: 'diamonds'}, {card: 9, value: 9, suit: 'clubs'}, {card: 10, value: 10, suit: 'hearts'}, {card: 10, value: 10, suit: 'spades'}, {card: 10, value: 10, suit: 'diamonds'}, {card: 10, value: 10, suit: 'clubs'}, {card: 'J', value: 10, suit: 'hearts'}, {card: 'J', value: 10, suit: 'spades'}, {card: 'J', value: 10, suit: 'diamonds'}, {card: 'J', value: 10, suit: 'clubs'}, {card: 'Q', value: 10, suit: 'hearts'}, {card: 'Q', value: 10, suit: 'spades'}, {card: 'Q', value: 10, suit: 'diamonds'}, {card: 'Q', value: 10, suit: 'clubs'}, {card: 'K', value: 10, suit: 'hearts'}, {card: 'K', value: 10, suit: 'spades'}, {card: 'K', value: 10, suit: 'diamonds'}, {card: 'K', value: 10, suit: 'clubs'}
 ]
 
 //build the deck with random cards
@@ -60,23 +60,39 @@ const checkHands = () => {
     alert('You tied!')
   }
 }
+//
 
 //Make the Start game button shuffle the deck
 $(() => {
+  const $userList = $('<ul>')
+  const $computerList = $('<ul>')
+  $('.user').append($userList)
+  $('.computer').append($computerList)
+
   $('#start').one('click', () => {
+    //shuffle the deck and make the deal button
     randomizeDeck()
     const $dealButton = $('<button>').text('Deal')
     $('body').append($dealButton)
+
     $($dealButton).on('click', () => {
+      //give each player two cards, turn the start button and the deal button off
       dealCards()
       $('#start').off('click')
       $($dealButton).off('click')
       console.log(userCards);
       console.log(computerCards);
-      const $userCurrentCards = $('<h3>').text(`User card value is ${countCardValue(userCards)}`)
-      $('#user').append($userCurrentCards)
-      const $computerCurrentCards = $('<h3>').text(`Computer card value is ${countCardValue(computerCards)}`)
-      $('#computer').append($computerCurrentCards)
+      //create list items which I can use each list item as a individual card?
+      // const $userLi = $('<li>')
+      // $userList.append($userLi)
+      // const $computerLi = $('<li>')
+      // $computerList.append($computerLi)
+      //display the users cards and value of their cards
+      const $userCurrentCardsValue = $('<h3>').text(`User has these cards ${userCards[0].card} of ${userCards[0].suit}, ${userCards[1].card} of ${userCards[1].suit} and the card value is ${countCardValue(userCards)}`)
+      $('#user').append($userCurrentCardsValue)
+      const $computerCurrentCardsValue = $('<h3>').text(`Computer has these cards ${computerCards[0].card} of ${computerCards[0].suit}, ${computerCards[1].card} of ${computerCards[1].suit} and the card value is ${countCardValue(computerCards)}`)
+      $('#computer').append($computerCurrentCardsValue)
+
       const $hitMeButton = $('<button>').text('Hit Me')
       $('body').append($hitMeButton)
       const $stayButton = $('<button>').text('Stay')
